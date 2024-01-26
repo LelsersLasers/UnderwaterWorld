336679b56dbd56a1eb6153cebfc65a3861627711
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2024-01-25 21:06:44 -0800

    Multiple X steps per frame

--- underwater_world/src/chunk.rs
+++ underwater_world/src/chunk.rs
@@ -7,6 +7,9 @@ pub const PERLIN_OCTAVES: u32 = 3;
 pub const ISO_LEVEL: f32 = -0.1;
 pub const MAX_HEIGHT: f32 = (CHUNK_SIZE * 2) as f32;
 
+const X_GENERATION_STEP_ISO: i32 = 9;
+const X_GENERATION_STEP_MESH: i32 = 8;
+
 const ISO_LEN: usize = CHUNK_SIZE + 1;
 
 enum BuildState {
@@ -77,116 +80,125 @@ impl Chunk {
     }
 
     fn build_iso(&mut self, perlin: &noise::Perlin) -> bool {
-        for y in 0..(CHUNK_SIZE + 1) {
-            for z in 0..(CHUNK_SIZE + 1) {
-                let iso = perlin_util::iso_at(
-                    perlin,
-                    (self.build.x + self.build.chunk_offset[0]) as f64 / CHUNK_SIZE as f64,
-                    (y as i32     + self.build.chunk_offset[1]) as f64 / CHUNK_SIZE as f64,
-                    (z as i32     + self.build.chunk_offset[2]) as f64 / CHUNK_SIZE as f64,
-                );
-                self.build.isos.push(iso);
+        for _ in 0..X_GENERATION_STEP_ISO {
+            for y in 0..(CHUNK_SIZE + 1) {
+                for z in 0..(CHUNK_SIZE + 1) {
+                    let iso = perlin_util::iso_at(
+                        perlin,
+                        (self.build.x + self.build.chunk_offset[0]) as f64 / CHUNK_SIZE as f64,
+                        (y as i32     + self.build.chunk_offset[1]) as f64 / CHUNK_SIZE as f64,
+                        (z as i32     + self.build.chunk_offset[2]) as f64 / CHUNK_SIZE as f64,
+                    );
+                    self.build.isos.push(iso);
+                }
             }
-        }
+    
+            self.build.x += 1;
 
-        self.build.x += 1;
+            if self.build.x == ISO_LEN as i32 { break; }
+        }
 
         self.build.x == ISO_LEN as i32
     }
 
     fn build_mesh(&mut self) -> bool {
-        let x = self.build.x as usize;
-        let chunk_offset = self.build.chunk_offset;
+        for _ in 0..X_GENERATION_STEP_MESH {
+            let x = self.build.x as usize;
+            let chunk_offset = self.build.chunk_offset;
 
-        for y in 0..CHUNK_SIZE {
-            for z in 0..CHUNK_SIZE {
+            for y in 0..CHUNK_SIZE {
+                for z in 0..CHUNK_SIZE {
 
-                let cube_corners = [
-                    [x, y, z],
-                    [x + 1, y, z],
-                    [x + 1, y, z + 1],
-                    [x, y, z + 1],
-                    [x, y + 1, z],
-                    [x + 1, y + 1, z],
-                    [x + 1, y + 1, z + 1],
-                    [x, y + 1, z + 1],
-                ];
-
-                let mut triangulation_idx = 0;
-                for (i, cube_corner) in cube_corners.iter().enumerate() {
-                    let iso_idx = corner_to_iso_idx(*cube_corner);
-                    let iso = self.build.isos[iso_idx];
-                    if iso < ISO_LEVEL {
-                        triangulation_idx |= 1 << i;
-                    }
-                }
-
-                let indices = marching_table::TRIANGULATION[triangulation_idx];
-
-
-                let mut pos_tris = Vec::with_capacity(indices.len() / 3);
-                let mut current_tri = Vec::with_capacity(3);
-
-
-                for (i, tri_index) in indices.iter().enumerate() {
-                    if *tri_index == -1 {
-                        let key = (x, y, z);
-                        self.build.tris.insert(key, pos_tris);
-                        break;
-                    }
-
-                    let edge_corners_indices = marching_table::EDGE_VERTEX_INDICES[*tri_index as usize];
-                    let corner_a_idx = edge_corners_indices[0];
-                    let corner_b_idx = edge_corners_indices[1];
-
-                    let corner_a = cube_corners[corner_a_idx];
-                    let corner_b = cube_corners[corner_b_idx];
-
-                    let iso_idx_a = corner_to_iso_idx(corner_a);
-                    let iso_idx_b = corner_to_iso_idx(corner_b);
-
-                    let iso_a = self.build.isos[iso_idx_a];
-                    let iso_b = self.build.isos[iso_idx_b];
-
-                    // interpolate using dist from iso
-                    let t = (ISO_LEVEL - iso_a) / (iso_b - iso_a);
-                    let corner_diff = [
-                        corner_b[0] as f32 - corner_a[0] as f32,
-                        corner_b[1] as f32 - corner_a[1] as f32,
-                        corner_b[2] as f32 - corner_a[2] as f32,
-                    ];
-                    let middle = [
-                        corner_a[0] as f32 + t * corner_diff[0],
-                        corner_a[1] as f32 + t * corner_diff[1],
-                        corner_a[2] as f32 + t * corner_diff[2],
+                    let cube_corners = [
+                        [x, y, z],
+                        [x + 1, y, z],
+                        [x + 1, y, z + 1],
+                        [x, y, z + 1],
+                        [x, y + 1, z],
+                        [x + 1, y + 1, z],
+                        [x + 1, y + 1, z + 1],
+                        [x, y + 1, z + 1],
                     ];
 
-                    let color_intensity = *tri_index as f32 / 16.0;
+                    let mut triangulation_idx = 0;
+                    for (i, cube_corner) in cube_corners.iter().enumerate() {
+                        let iso_idx = corner_to_iso_idx(*cube_corner);
+                        let iso = self.build.isos[iso_idx];
+                        if iso < ISO_LEVEL {
+                            triangulation_idx |= 1 << i;
+                        }
+                    }
 
-                    let vert = draw::VertColor::new(
-                        [
-                            middle[0] + chunk_offset[0] as f32,
-                            middle[1] + chunk_offset[1] as f32,
-                            middle[2] + chunk_offset[2] as f32,
-                        ],
-                        [0.7, color_intensity, color_intensity],
-                    );
-                    self.build.verts.push(vert);
+                    let indices = marching_table::TRIANGULATION[triangulation_idx];
 
 
-                    current_tri.push(vert.pos);
-                    if i % 3 == 2 {
-                        let tri = util::Tri::new([current_tri[0], current_tri[1], current_tri[2]]);
-                        pos_tris.push(tri);
-                        current_tri.clear();
+                    let mut pos_tris = Vec::with_capacity(indices.len() / 3);
+                    let mut current_tri = Vec::with_capacity(3);
+
+
+                    for (i, tri_index) in indices.iter().enumerate() {
+                        if *tri_index == -1 {
+                            let key = (x, y, z);
+                            self.build.tris.insert(key, pos_tris);
+                            break;
+                        }
+
+                        let edge_corners_indices = marching_table::EDGE_VERTEX_INDICES[*tri_index as usize];
+                        let corner_a_idx = edge_corners_indices[0];
+                        let corner_b_idx = edge_corners_indices[1];
+
+                        let corner_a = cube_corners[corner_a_idx];
+                        let corner_b = cube_corners[corner_b_idx];
+
+                        let iso_idx_a = corner_to_iso_idx(corner_a);
+                        let iso_idx_b = corner_to_iso_idx(corner_b);
+
+                        let iso_a = self.build.isos[iso_idx_a];
+                        let iso_b = self.build.isos[iso_idx_b];
+
+                        // interpolate using dist from iso
+                        let t = (ISO_LEVEL - iso_a) / (iso_b - iso_a);
+                        let corner_diff = [
+                            corner_b[0] as f32 - corner_a[0] as f32,
+                            corner_b[1] as f32 - corner_a[1] as f32,
+                            corner_b[2] as f32 - corner_a[2] as f32,
+                        ];
+                        let middle = [
+                            corner_a[0] as f32 + t * corner_diff[0],
+                            corner_a[1] as f32 + t * corner_diff[1],
+                            corner_a[2] as f32 + t * corner_diff[2],
+                        ];
+
+                        let color_intensity = *tri_index as f32 / 16.0;
+
+                        let vert = draw::VertColor::new(
+                            [
+                                middle[0] + chunk_offset[0] as f32,
+                                middle[1] + chunk_offset[1] as f32,
+                                middle[2] + chunk_offset[2] as f32,
+                            ],
+                            [0.7, color_intensity, color_intensity],
+                        );
+                        self.build.verts.push(vert);
+
+
+                        current_tri.push(vert.pos);
+                        if i % 3 == 2 {
+                            let tri = util::Tri::new([current_tri[0], current_tri[1], current_tri[2]]);
+                            pos_tris.push(tri);
+                            current_tri.clear();
+                        }
                     }
                 }
             }
+
+
+            self.build.x += 1;
+
+            if self.build.x == CHUNK_SIZE as i32 { break; }
         }
 
         self.build.num_verts = self.build.verts.len();
-
-        self.build.x += 1;
 
         self.build.x == CHUNK_SIZE as i32
     }
