542985b3a59b38c672118eaba557a6a6daf66978
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-01-30 09:05:28 -0800

    Update consts.rs

--- underwater_world/src/consts.rs
+++ underwater_world/src/consts.rs
@@ -7,10 +7,3 @@ pub const CLEAR_COLOR: wgpu::Color = wgpu::Color {
     b: 0.03310476657,
     a: 1.0,
 };
-
-// pub const CLEAR_COLOR: wgpu::Color = wgpu::Color {
-//     r: 0.3,
-//     g: 0.3,
-//     b: 0.3,
-//     a: 1.0,
-// };
